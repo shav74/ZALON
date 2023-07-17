113355639fa95a8a5177295fb89c7fa6d3ccc166
shav74 <101064560+shav74@users.noreply.github.com>
2023-07-17 17:28:03 +0530

    Create footer.php

---
+++ inc/footer.php
@@ -0,0 +1,68 @@
+<footer style="background:#e0dad5; position: relative; bottom:0; right:0">
+  <!-- <div class="footer" style="background:#e0dad5"> -->
+  <div class="main-content">
+    <div class="left box">
+      <h2>About us</h2>
+      <div class="content">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+          Voluptate, eos corrupti repudiandae sint quas quod? Molestias quae
+          voluptas vero ipsam aperiam, itaque assumenda quia nihil, beatae
+          ut excepturi, cupiditate consequatur.
+        </p>
+        <div class="social">
+          <a href="https://facebook.com/coding.np"><span class="fab fa-facebook-f"></span></a>
+          <a href="#"><span class="fab fa-twitter"></span></a>
+          <a href="https://instagram.com/coding.np"><span class="fab fa-instagram"></span></a>
+          <a href="https://youtube.com/c/codingnepal"><span class="fab fa-youtube"></span></a>
+        </div>
+      </div>
+    </div>
+
+    <div class="center box">
+      <h2>Address</h2>
+      <div class="content">
+        <div class="place">
+          <span class="fas fa-map-marker-alt"></span>
+          <span class="text">Birendranagar, Surkhet</span>
+        </div>
+        <div class="phone">
+          <span class="fas fa-phone-alt"></span>
+          <span class="text">+089-765432100</span>
+        </div>
+        <div class="email">
+          <span class="fas fa-envelope"></span>
+          <span class="text">abc@example.com</span>
+        </div>
+      </div>
+    </div>
+
+    <div class="right box">
+      <h2>Contact us</h2>
+      <div class="content">
+        <form action="#">
+          <div class="email">
+            <div class="text">Email *</div>
+            <input type="email" required />
+          </div>
+          <div class="msg">
+            <div class="text">Message *</div>
+            <textarea rows="2" cols="25" required></textarea>
+          </div>
+          <div class="btn">
+            <button type="submit">Send</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+  <div class="bottom">
+    <center>
+      <span class="credit">
+        <a href="https://www.codingnepalweb.com">ZALON.LK</a> |
+      </span>
+      <span class="far fa-copyright"></span><span> 2023 All rights reserved.</span>
+    </center>
+  </div>
+  <!-- </div> -->
+</footer>
